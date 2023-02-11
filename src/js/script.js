871dcaec978268b1428eb119e46de9760d2cbf1b
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-11 10:42:37 +0200

    add gulp files

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+let slider = $(document).ready(function () {
+  $('.your-class').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    infinite: true
+  });
+});
+
