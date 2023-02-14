0fd9b14328cde86ec03d5ec8bfe1796a59de8814
melkoff <vasamelko@gmail.com>
2023-02-14 16:05:20 +0200

    add some code to story section

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,27 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+// let slider = $(document).ready(function () {
+//   $('.your-class').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     dots: true,
+//     infinite: true
+//   });
+// });
 
+// More Text in section OUR-STORY!!! //
+function ourStory() {
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more-story");
+  let btnText = document.getElementById("myBtnStory");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Less";
+    moreText.style.display = "inline";
+  }
+}
+// ================================================================= //
