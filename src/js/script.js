64938389cbd9f979e8db41dc737addd5e853eee0
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-24 17:08:14 +0200

    script-our-menu

--- src/js/script.js
+++ src/js/script.js
@@ -182,3 +182,24 @@ function moreText2() {
   }
 }
 // ================================================================= //
+/**popular menu section our menu btn */
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("openModal");
+let closeBtn = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  btn.style.display = "none";
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+  btn.style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    btn.style.display = "block";
+  }
+}
