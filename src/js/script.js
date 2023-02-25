f19270778abbcc50a1f362fc37d41201d734b0a1
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-25 23:04:26 +0200

    burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,21 @@
-// let slider = $(document).ready(function () {
-//   $('.your-class').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: true,
-//     dots: true,
-//     infinite: true
-//   });
-// });
+//Mobile menu//
+let humb = document.querySelector('#burger-icon');
+let humbBody = document.querySelector('#burger-menu');
+let humbLink = document.querySelector('#burger-menu');
+humb.addEventListener('click', humbHandler);
+function humbHandler(e) {
+   e.preventDefault();
+   humbBody.classList.toggle("open");
+   humb.classList.toggle('active');
+   humbLink.classList.toggle('active');
+}
+$('.burger__menu a').on('click', function(e){
+  $('.burger__menu').toggleClass('open');
+  $('#burger-icon').toggleClass('active');
+})
+
+
+
 // Our-Story-Slider //
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
