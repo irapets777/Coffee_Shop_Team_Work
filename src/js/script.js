9b6e1a2ea5e192afb78d21ed3915e604c61ac8bd
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-28 23:15:20 +0200

    burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,22 @@
+//Mobile menu//
+let humb = document.querySelector('#burger-icon');
+let humbBody = document.querySelector('#burger-menu');
+let humbLink = document.querySelector('#burger-menu');
+humb.addEventListener('click', humbHandler);
+function humbHandler(e) {
+   e.preventDefault();
+   humbBody.classList.toggle("open");
+   humb.classList.toggle('active');
+   humbLink.classList.toggle('active');
+  }
+
+$('.burger__menu a').on('click', function(e){
+  $('.burger__menu').toggleClass('open');
+  $('#burger-icon').toggleClass('active');
+})
+
+
+
 // Our-Story-Slider //
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
